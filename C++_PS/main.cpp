6330471f63df1727cee4f44c28598f380c116376
Zedd0202 <ch41150@hanmail.net>
2017-11-15 13:59:01 +0900

    create C++_PS repo

---
+++ C++_PS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++_PS
+//
+//  Created by Zedd on 2017. 11. 15..
+//  Copyright © 2017년 Zedd. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
